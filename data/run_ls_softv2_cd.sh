8a33d4952f5cd603bbee6c9173a009a72220c52e
ZhuangNing <90459351+zncola@users.noreply.github.com>
2024-10-14 16:23:58 +0800

    Updating run_ls_softv2_cd.sh

---
+++ data/run_ls_softv2_cd.sh
@@ -0,0 +1,2 @@
+ython /Work21/2023/zhuangning/code/prompt-gpt/nlutrain_ls_soft2_cd.py --dataset1 banking --dataset2 hotels --batch_size 8 --template_choice 3 --verbalizer_choice 6 --epoch 50 --maxlength 256 --cuda '1' --warmup_step 1000
+python /Work21/2023/zhuangning/code/prompt-gpt/nlutrain_ls_soft2_cd.py --dataset1 hotels --dataset2 banking --batch_size 8 --template_choice 3 --verbalizer_choice 6 --epoch 50 --maxlength 256 --cuda '1' --warmup_step 1000
